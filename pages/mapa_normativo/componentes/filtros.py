6b5966b68b87b15932268a6404623e9a15b2a9c7
Pano Valdez <marianovaldez92@protonmail.com>
2023-08-09 17:02:10 -0300

    cambio de lugar carpeta de data de mapa normativo

--- pages/mapa_normativo/componentes/filtros.py
+++ pages/mapa_normativo/componentes/filtros.py
@@ -3,7 +3,7 @@ from dash import html, dcc
 import dash_bootstrap_components as dbc
 import dash_daq as daq
 
-from data import MUNICIPIOS
+from ..data import MUNICIPIOS
 
 Filtros = html.Div(
     [
