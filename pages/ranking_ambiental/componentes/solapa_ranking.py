4756e29d7f7ed966350d18d5096208199b817806
Agu Collomb <agucollomb@gmail.com>
2023-08-25 12:53:03 -0300

    SEGUIMOS CON EL ARMADO RANKING

--- pages/ranking_ambiental/componentes/solapa_ranking.py
+++ pages/ranking_ambiental/componentes/solapa_ranking.py
@@ -4,8 +4,8 @@ from dash import Dash
 from .transparencia import Transparencia
 from .texto_inicial_ranking import TextoRanking_2
 
-color_transparencia = '#EF7286'
-color_escuelas = '#FF865F'
+color_transparencia = '#FF865F'
+color_escuelas = '#EF7286'
 color_agua = '#6FBFB5'
 color_poblaciones = '#D8D87C'
 color_apiarios = '#C3A4E7'
@@ -15,12 +15,12 @@ color_agroecologia= '#9AD5FF'
 SolapasRanking = html.Div([
     dbc.Row([
         dbc.Tabs(id="tabs-ranking", active_tab='tab-A', children=[
-            dbc.Tab(label='ESCUELAS', tab_id='tab-A', label_style={"color":'#FFFFFF' , 'font-size': '18px', 'background-color':color_transparencia,  'border-top-left-radius': '40px','border-bottom-left-radius': '40px','border-bottom-right-radius': '40px','border-top-right-radius': '40px',  "text-align": "center",'width': '186px'},active_label_style={'border': '4px solid white'}, className='nav-tabs-custom'),
-            dbc.Tab(label='TRANSPARENCIAS', tab_id='tab-B', label_style={"color":'#FFFFFF' , 'font-size': '18px', 'background-color':color_escuelas,  'border-top-left-radius': '40px','border-bottom-left-radius': '40px','border-bottom-right-radius': '40px','border-top-right-radius': '40px',  "text-align": "center",'width': '186px'},active_label_style={'border': '4px solid white'}, className='nav-tabs-custom'),
-            dbc.Tab(label='AGUA', tab_id='tab-C', label_style={"color":'#FFFFFF' , 'font-size': '18px', 'background-color':color_agua,  'border-top-left-radius': '40px','border-bottom-left-radius': '40px','border-bottom-right-radius': '40px','border-top-right-radius': '40px',  "text-align": "center",'width': '186px'},active_label_style={'border': '4px solid white'}, className='nav-tabs-custom'),
-            dbc.Tab(label='POBLACIONES', tab_id='tab-D', label_style={"color":'#FFFFFF' , 'font-size': '18px', 'background-color':color_poblaciones,  'border-top-left-radius': '40px','border-bottom-left-radius': '40px','border-bottom-right-radius': '40px','border-top-right-radius': '40px',  "text-align": "center",'width': '186px'},active_label_style={'border': '4px solid white'}, className='nav-tabs-custom'),
-            dbc.Tab(label='APIARIOS', tab_id='tab-E', label_style={"color":'#FFFFFF' , 'font-size': '18px', 'background-color':color_apiarios,  'border-top-left-radius': '40px','border-bottom-left-radius': '40px','border-bottom-right-radius': '40px','border-top-right-radius': '40px',  "text-align": "center",'width': '186px'},active_label_style={'border': '4px solid white'}, className='nav-tabs-custom'),
-            dbc.Tab(label='AGROECOLOGÍA', tab_id='tab-F', label_style={"color":'#FFFFFF' , 'font-size': '18px', 'background-color':color_agroecologia,  'border-top-left-radius': '40px','border-bottom-left-radius': '40px','border-bottom-right-radius': '40px','border-top-right-radius': '40px', "text-align": "center",'width': '186px'},active_label_style={'border': '4px solid white'}, className='nav-tabs-custom'),
+            dbc.Tab(label='ESCUELAS', tab_id='tab-A', label_style={"color":"#000000" , 'font-size': '18px', 'background-color':color_escuelas,  'border-top-left-radius': '40px','border-bottom-left-radius': '40px','border-bottom-right-radius': '40px','border-top-right-radius': '40px',  "text-align": "center",'width': '186px'},active_label_style={'border': '4px solid white'}, className='nav-tabs-custom'),
+            dbc.Tab(label='TRANSPARENCIAS', tab_id='tab-B', label_style={"color":"#000000" , 'font-size': '18px', 'background-color':color_transparencia,  'border-top-left-radius': '40px','border-bottom-left-radius': '40px','border-bottom-right-radius': '40px','border-top-right-radius': '40px',  "text-align": "center",'width': '186px'},active_label_style={'border': '4px solid white'}, className='nav-tabs-custom'),
+            dbc.Tab(label='AGUA', tab_id='tab-C', label_style={"color":"#000000" , 'font-size': '18px', 'background-color':color_agua,  'border-top-left-radius': '40px','border-bottom-left-radius': '40px','border-bottom-right-radius': '40px','border-top-right-radius': '40px',  "text-align": "center",'width': '186px'},active_label_style={'border': '4px solid white'}, className='nav-tabs-custom'),
+            dbc.Tab(label='POBLACIONES', tab_id='tab-D', label_style={"color":"#000000" , 'font-size': '18px', 'background-color':color_poblaciones,  'border-top-left-radius': '40px','border-bottom-left-radius': '40px','border-bottom-right-radius': '40px','border-top-right-radius': '40px',  "text-align": "center",'width': '186px'},active_label_style={'border': '4px solid white'}, className='nav-tabs-custom'),
+            dbc.Tab(label='APIARIOS', tab_id='tab-E', label_style={"color":"#000000" , 'font-size': '18px', 'background-color':color_apiarios,  'border-top-left-radius': '40px','border-bottom-left-radius': '40px','border-bottom-right-radius': '40px','border-top-right-radius': '40px',  "text-align": "center",'width': '186px'},active_label_style={'border': '4px solid white'}, className='nav-tabs-custom'),
+            dbc.Tab(label='AGROECOLOGÍA', tab_id='tab-F', label_style={"color":"#000000" , 'font-size': '18px', 'background-color':color_agroecologia,  'border-top-left-radius': '40px','border-bottom-left-radius': '40px','border-bottom-right-radius': '40px','border-top-right-radius': '40px', "text-align": "center",'width': '186px'},active_label_style={'border': '4px solid white'}, className='nav-tabs-custom'),
         ], style={'borderBottom': '0px', 'width': '100%'})
     ]),
     html.Div(id='tabs-content-ranking'),
