89f40a433848fc1bd19b9b56801a9220ea6f84ff
Pano Valdez <marianovaldez92@protonmail.com>
2023-10-20 11:31:13 -0300

    retoques en interfaz

--- pages/mapa_normativo/componentes/filtros.py
+++ pages/mapa_normativo/componentes/filtros.py
@@ -31,34 +31,36 @@ Filtros = html.Div(
         ]),
         html.Br(),
         dbc.Row([
-            dbc.Col(daq.BooleanSwitch(on=True, id="toggle_cursos", labelPosition="bottom",color=CURSOS_CUERPOS_AGUA), md=3),
-            dbc.Col(html.Span("Cursos de agua"), md=9)
+            dbc.Col(daq.BooleanSwitch(on=True, id="toggle_cursos", labelPosition="bottom",color=CURSOS_CUERPOS_AGUA), xs=3),
+            dbc.Col(html.Span("Cursos de agua"), xs=9)
         ], className="text-white mb-2"),
         dbc.Row([
-            dbc.Col(daq.BooleanSwitch(on=True, id="toggle_cuerpos", labelPosition="bottom",color=CURSOS_CUERPOS_AGUA), md=3),
-            dbc.Col(html.Span("Cuerpos de agua"), md=9)
+            dbc.Col(daq.BooleanSwitch(on=True, id="toggle_cuerpos", labelPosition="bottom",color=CURSOS_CUERPOS_AGUA), xs=3),
+            dbc.Col(html.Span("Cuerpos de agua"), xs=9)
         ], className="text-white mb-2"),
         dbc.Row([
-            dbc.Col(daq.BooleanSwitch(on=True, id="toggle_localidades", labelPosition="bottom",color=LOCALIDADES), md=3),
-            dbc.Col(html.Span("Localidades"), md=9)
+            dbc.Col(daq.BooleanSwitch(on=True, id="toggle_localidades", labelPosition="bottom",color=LOCALIDADES), xs=3),
+            dbc.Col(html.Span("Localidades"), xs=9)
         ], className="text-white mb-2"),
         dbc.Row([
-            dbc.Col(daq.BooleanSwitch(on=True, id="toggle_reservas", labelPosition="bottom",color=RESERVAS), md=3),
-            dbc.Col(html.Span("Reservas"), md=9)
+            dbc.Col(daq.BooleanSwitch(on=True, id="toggle_reservas", labelPosition="bottom",color=RESERVAS), xs=3),
+            dbc.Col(html.Span("Reservas"), xs=9)
         ], className="text-white mb-2"),
         dbc.Row([
-            dbc.Col(daq.BooleanSwitch(on=True, id="toggle_escuelas", labelPosition="bottom",color=ESCUELAS), md=3),
-            dbc.Col(html.Span("Escuelas Rurales"), md=9)
+            dbc.Col(daq.BooleanSwitch(on=True, id="toggle_escuelas", labelPosition="bottom",color=ESCUELAS), xs=3),
+            dbc.Col(html.Span("Escuelas Rurales"), xs=9)
         ], className="text-white mb-2"),
         dbc.Row([
-            dbc.Col(daq.BooleanSwitch(on=True, id="toggle_excl", labelPosition="bottom",color=ZONA_EXCLUSIÓN), md=3),
-            dbc.Col(html.Span("Zonas de Exclusión"), md=9)
+            dbc.Col(daq.BooleanSwitch(on=True, id="toggle_excl", labelPosition="bottom",color=ZONA_EXCLUSIÓN), xs=3),
+            dbc.Col(html.Span("Zonas de Exclusión"), xs=9)
         ], className="text-white mb-2"),
         dbc.Row([
-            dbc.Col(daq.BooleanSwitch(on=True, id="toggle_amort", labelPosition="bottom",color=ZONA_AMORTIGUAMIENTO), md=3),
-            dbc.Col(html.Span("Zonas de Amortiguamiento"), md=9)
+            dbc.Col(daq.BooleanSwitch(on=True, id="toggle_amort", labelPosition="bottom",color=ZONA_AMORTIGUAMIENTO), xs=3),
+            dbc.Col(html.Span("Zonas de Amortiguamiento"), xs=9)
         ], className="text-white mb-2"),
-        
+        dbc.Row(
+            dbc.Col(html.A(html.Img(src="assets/img/flechita.svg", className="animate__animated animate__fadeInDown animate__slower animate__infinite"), href="#footer-normativo"), class_name="text-center mt-5")
+        )
         
     ],
     id="filtros",
