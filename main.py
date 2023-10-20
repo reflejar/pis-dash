2754965c13fd61deb6be688b729e92a028e1bb49
Pano Valdez <marianovaldez92@protonmail.com>
2023-10-20 13:46:01 -0300

    dividimos analytics y agregamos logs

--- main.py
+++ main.py
@@ -1,37 +1,51 @@
 import sys
+import importlib
+import os
+import datetime
+from logger import logger
 
 ############################
 ### Se inicia Flask
 ############################
 
 from flask import Flask, request
+
 server = Flask(__name__)
-import dash
+
 
 ############################
 ### Se inicia Dash
 ############################
 
+import dash
 import dash_bootstrap_components as dbc
 from dash import html, Output, Input, dcc
-
-from pages import (
-	mapa_normativo,
-	ranking_ambiental,
-	indicadores_censos
-)
 
 # Se crea Dash y elegimos el tema
 app = dash.Dash(
 	__name__,
 	server=server,
-	external_stylesheets=[dbc.icons.BOOTSTRAP, 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css'], # COSMO, FLATLY, LUX, MINTY
-	use_pages=True,
+	external_stylesheets=[dbc.icons.BOOTSTRAP, 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css'],
+	use_pages=False,
 	update_title="Actualizando...",
 	prevent_initial_callbacks=True,
 	title="PIS",
 	suppress_callback_exceptions=True,
 )
+
+# Se elige la herramienta
+try:
+    APP_CONFIG = os.environ["APP_CONFIG"]
+    herramienta = importlib.import_module(f'.{APP_CONFIG}', 'pages')
+except:
+    APP_CONFIG = sys.argv[1] if len(sys.argv) > 1 else 'indicadores_censos'
+    herramienta = importlib.import_module(f'.{APP_CONFIG}', 'pages')
+
+# Analytics
+with open('index.html', 'r') as file:
+    html_content = file.read()
+    GTAG_ANALYTICS = os.environ.get("GTAG_ANALYTICS", "")
+    app.index_string = html_content.replace('GTAG_ANALYTICS', GTAG_ANALYTICS)
 
 # Se agregan los componentes de la web
 ISOLOGOTIPO = html.Img(src="assets/img/PIS_isologo_negro.png", alt="Isotipo de PIS", height="70px")
@@ -48,7 +62,7 @@ app.layout = html.Div(children=[
         fixed="top",
         className="text-primary",
     ),
-	html.Div(id='page-content'),
+    html.Div(id='contenido-herramienta'),
 	html.Footer([
         dbc.Container([
             dbc.Row([
@@ -67,53 +81,17 @@ app.layout = html.Div(children=[
         ],
         className="text-white position-relative py-4",
         id="footer"
-    )])
+    )]),
+    
 ])
 
-
-
-@app.callback(Output('page-content', 'children'),Input('url', 'pathname'))
-def display_page(_):
-
-    sections = {
-            'zonificacion': {'layout': mapa_normativo.layout, 'analytics': 'G-4BW9V2HCS0'},
-            'censos': {'layout': indicadores_censos.layout, 'analytics': 'G-P4RK1GKTG0'},
-            'ranking': {'layout': ranking_ambiental.layout, 'analytics': 'G-FBLLE0SDB7'}
-    }
-
-    try:
-        page_config = sections[request.host.split(".")[0]]
-    except:
-        page_config = sections[sys.argv[1]] if len(sys.argv) > 1 else sections['censos']
-    
-    app.index_string = """<!DOCTYPE html>
-        <html>
-            <head>
-                <!-- Google tag (gtag.js) --> """ + \
-                f"""<script async src="https://www.googletagmanager.com/gtag/js?id={page_config['analytics']}"></script>""" + \
-                """<script>
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());""" + \
-                f"""gtag('config', '{page_config['analytics']}');
-                </script>""" + \
-                """{%metas%}
-                <title>{%title%}</title>
-                {%favicon%}
-                {%css%}
-            </head>
-            <body>
-                {%app_entry%}
-                <footer>
-                    {%config%}
-                    {%scripts%}
-                    {%renderer%}
-                </footer>
-            </body>
-        </html>""" 
-    return page_config['layout']
-
-
+@app.callback(Output('contenido-herramienta', 'children'),Input('url', 'pathname'))
+def display_page(url):
+    # Fecha y hora actual
+    now = datetime.datetime.now()
+    logger.info(f'{request.host} {url} - {now.strftime("%Y-%m-%d %H:%M:%S")}')
+    # Fecha y hora actual
+    return herramienta.layout
 # Se corre la aplicación
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=8050, debug=True, use_reloader=True)
