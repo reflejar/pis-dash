4ba7448850ecf699cc83a33019df408de39daae4
Pano Valdez <marianovaldez92@protonmail.com>
2023-11-08 13:12:38 -0300

    cambio textos

--- pages/ranking_ambiental/componentes/encabezado.py
+++ pages/ranking_ambiental/componentes/encabezado.py
@@ -3,9 +3,9 @@ import dash_bootstrap_components as dbc
 
 Encabezado = dbc.Row([
                 dbc.Col([
-                    html.H3("Ranking",className="text-white fw-bolder pt-3"),
+                    html.H3(["Ranking", html.A("V 1.0", href="#metodologia-ranking", className="btn btn-sm text-primary")],className="text-white pt-3 fw-bolder space-grotesk"),
                     html.Br(),
-                    html.P(["Clasificación interactiva de ordenanzas municipales de regulación de agroquímicos y promoción de la agroecología. Seleccioná la dimensión analítica de tu interés y compara entre municipios"], className="text-white poppins"),
-                    html.P(html.A("V 1.0 (métodologia & producto)", href="#metodologia-ranking", className="text-primary text-uppercase"))],  md=12, class_name="text-white mt-5"),
+                    html.P(["Clasificación interactiva de ordenanzas municipales de regulación de agroquímicos y promoción de la agroecología bonaerense. Seleccioná la dimensión analítica de tu interés y compara entre municipios"], className="text-white poppins"),
+                ],  md=12, class_name="text-white mt-5"),
             ], class_name="my-5")
 
--- pages/ranking_ambiental/componentes/textos.py
+++ pages/ranking_ambiental/componentes/textos.py
@@ -19,15 +19,16 @@ import dash_bootstrap_components as dbc
 
 Metodologia = dbc.Row(dbc.Col([
         html.H5([html.Strong("METODOLOGÍA & PRODUCTO"), html.Span("V1.0",className='badge bg-primary text-black mx-3')], className="text-white pt-3 space-grotesk"),
-        html.P("En la Argentina los municipios poseen (o no) ordenanzas que regulan y zonifican el uso de productos agroquímicos en sus territorios, Normativa Comparada es una sistematización de todas las normativas de promoción a la agroecologia y de regulación de agroquímicos."),
-        html.P("Para el comparativo de normativas se establecieron cinco dimensiones analíticas: Transparencia , Poblaciones , Escuelas Rurales , Agua y Apiarios."),
-        html.P("No todas las ordenanzas poseen todos los puntos, ni responden a las mismas realidades territoriales, por lo que se analiza cada dimensión de manera diferenciada. Las dimensiones analíticas permiten ordenar las ordenanzas municipales de mayor a menor y viceversa, pero como toda selección de criterios esta puede ser arbitraria. El objetivo del mismo no es dar cuenta en términos absolutos de que una ordenanza efectivamente protege mejor su territorio que otra, sino ser una herramienta de fácil uso para analizar los distintos niveles de protección ambiental existentes. Esta herramienta fue pensada para: "),
+        html.P('En la Argentina los municipios poseen (o no) ordenanzas que egulan el uso de agroquímicos y/o promociones la agroecología en sus territorios. "Ranking" es una sistematización de todas las normativas de regulación de agroquímicos y de promoción a la agroecologia.'),
+        html.P('Para facilitar el analisis normativo se establecieron 6 dimensiones: Transparencia, Poblaciones, Escuelas Rurales, Agua, Apiarios y agroecologia. No todos los municipios legislan todos los puntos, ni responden a las mismas realidades territoriales, por lo que se analiza cada dimensión de manera diferenciada. Las dimensiones analíticas permiten ordenar las ordenanzas municipales de mayor a menor y viceversa, pero como toda selección de criterios esta puede ser arbitraria.'),
+        html.P('El objetivo del mismo no es dar cuenta en términos absolutos de que una ordenanza efectivamente protege mejor su territorio que otra, sino ser una herramienta de fácil uso para analizar los distintos niveles de protección ambiental existentes. Esta herramienta fue pensada para: '),
         html.Ul([
-            html.Li("Inspirar a los municipios que no tienen ordenanza"),
-            html.Li("Mejorar las ordenanzas vigentes"),
-            html.Li("Estudiar el marco legal en cada distrito. Esta información fue relevada de manera colectivo, participativa y abierta. Si encontraste algún error o información desactualizada comunícate a contacto."),
+            html.Li('Inspirar a los municipios que no tienen ordenanza'),
+            html.Li('Mejorar las ordenanzas vigentes'),
+            html.Li('Entender el marco legal en cada distrito'),
         ]),
-        html.P("También se puede acceder al texto de cada una de las ordenanzas y al dataset utilizado para la construcción de la herramienta")
+        html.P('Esta información fue relevada de manera colectiva, participativa y abierta.'),
+        html.P('Si encontraste algún error o información desactualizada comunícate a contacto. También se puede acceder al texto de cada una de las ordenanzas haciendo click sobre el numero de la misma, dentro de la tabla. Priorizamos links a repositorios oficiales. En el caso de no existir se linkea al texto de la ordenanza alojado en servidores propios. El dataset utilizado para la construcción de la herramienta es de libre acceso, podes encontrarlo acá.'),
     ],
     id="metodologia-ranking",
     className=" text-white mt-5"
