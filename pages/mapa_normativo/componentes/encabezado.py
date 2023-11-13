d46873c1caaf0a0819483adecd81778c04d36070
Pano Valdez <marianovaldez92@protonmail.com>
2023-11-13 18:05:44 -0300

    pequeño detalle

--- pages/mapa_normativo/componentes/encabezado.py
+++ pages/mapa_normativo/componentes/encabezado.py
@@ -5,7 +5,7 @@ Encabezado = dbc.Row([
                 dbc.Col([
                     html.H3(['Zonificación', html.A("V 1.0", href="#metodologia-normativa", className="btn btn-sm text-primary")], className="text-white pt-3 fw-bolder space-grotesk"),
                     html.Br(),
-                    html.P(["""Mapa de Áreas pobladas, escuelas rurales , cursos y superficies e agua, zonas de exclusión y 
+                    html.P(["""Mapa de Áreas pobladas, escuelas rurales, cursos y superficies de agua, zonas de exclusión y 
                             amortiguamiento según normativa vigente.""", html.Br(), """Seleccioná el municipio de tu interés, activá y desactivá las capas para ver el ordenamiento territorial."""
                     ], className="text-white poppins"),
 
